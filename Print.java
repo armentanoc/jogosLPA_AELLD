775faf7f7f49c513084b9d9b0fd974575acf0852
kérous <88147887+armentanoc@users.noreply.github.com>
2022-06-03 10:26:36 -0300

    Update Print.java

--- Print.java
+++ Print.java
@@ -28,6 +28,9 @@ public class Print {
 	}
 	
 	public static void tabDamas(String[][] tabuleiro) {
+		
+		System.out.println("");
+		
 		for (int i = 0; i < tabuleiro.length; i++) {
 			for (int j = 0; j < tabuleiro[i].length; j++) {
 				System.out.print(tabuleiro[i][j] + "");}
